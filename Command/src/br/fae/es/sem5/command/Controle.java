cc6469dff6a0335e6feb55d86e9f9ab7825a7434
gilsonmaran <gilson.maran@gmail.com>
2017-05-23 12:05:26 -0300

    Pequenas Correções

--- Command/src/br/fae/es/sem5/command/Controle.java
+++ Command/src/br/fae/es/sem5/command/Controle.java
@@ -14,7 +14,7 @@ public class Controle {
 	public String listarHistorico() {
             StringBuilder builder = new StringBuilder();
 
-            for (String h : historico)
+            for(String h : historico)
                 builder.append(h);
 
 		return builder.toString();
