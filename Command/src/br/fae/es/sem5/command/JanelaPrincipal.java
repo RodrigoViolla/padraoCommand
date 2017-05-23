73d25ef74e5a6b623b6afe29cd3b31e756b007c5
Coyoteee <gabriel.lins.alves@gmail.com>
2017-05-22 23:55:30 -0300

    Beta Release v0.5
    Correção geral da janela
    By: Gilson Maran

--- Command/src/br/fae/es/sem5/command/JanelaPrincipal.java
+++ Command/src/br/fae/es/sem5/command/JanelaPrincipal.java
@@ -12,73 +12,215 @@ import javax.swing.SwingConstants;
 
 public class JanelaPrincipal extends JFrame {
 	private static final long serialVersionUID = 1L;
+        Controle gerenciar = new Controle();
+        
+        Dispositivo tv = new Dispositivo("TV");
+        Dispositivo radio = new Dispositivo("Radio");
+        Dispositivo portaoEletronico = new Dispositivo("Portão Eletrônico");
+        Dispositivo arCondicionado = new Dispositivo("Ar Condicionado");
 
 	public JanelaPrincipal() {
 		super("Controle Remoto");
 		
-		setBackground(new Color(169, 169, 169));
-		setLayout(null);
+                /*-----------------------------------------------------------
+                CRIAÇÃO DOS COMPONENTES
+                -----------------------------------------------------------*/
+                JLabel lblTitulo = new JLabel("Controle Remoto");
+                JLabel lblResposta = new JLabel("Nenhuma Ação Realizada");
+                JLabel lblHistorico = new JLabel("Histórico Vazio");
+                
+                JLabel lblTv = new JLabel("TV");
+                JLabel lblRadio = new JLabel("Radio");
+                JLabel lblPortaoEletronico = new JLabel("Port�o Eletronico");
+                JLabel lblArCondicionado = new JLabel("Ar Condicionado");
+                
+                JButton btnLigarTV = new JButton("Ligar");
+                JButton btnDesligarTV = new JButton("Desligar");
+                JButton btnLigarRadio = new JButton("Ligar");
+                JButton btnDesligarRadio = new JButton("Desligar");
+                JButton btnLigarPortaoEletronico = new JButton("Ligar");
+                JButton btnDesligarPortaoEletronico = new JButton("Desligar");
+                JButton btnLigarArCondicionado = new JButton("Ligar");
+                JButton btnDesligarArCondicionado = new JButton("Desligar");
+                
+                
+                /*-----------------------------------------------------------
+                DEFINIÇÃO DO TAMANHO DOS COMPONENTES
+                -----------------------------------------------------------*/
+                lblTitulo.setBounds(60, 34, 230, 23);
+                lblResposta.setBounds(10, 400, 200, 23);
+                lblHistorico.setBounds(10, 420, 420, 23);
+                
+                lblTv.setBounds(10, 102, 132, 23);
+                lblRadio.setBounds(10, 172, 132, 23);
+		lblRadio.setBounds(10, 172, 132, 23);
+                lblArCondicionado.setBounds(10, 300, 132, 23);
+                lblPortaoEletronico.setBounds(10, 237, 132, 23);
+               
+                btnLigarTV.setBounds(152, 102, 89, 23);
+		btnDesligarTV.setBounds(251, 102, 89, 23);
+                btnLigarRadio.setBounds(152, 172, 89, 23);
+		btnDesligarRadio.setBounds(251, 172, 89, 23);
+                btnLigarPortaoEletronico.setBounds(152, 237, 89, 23);
+		btnDesligarPortaoEletronico.setBounds(251, 237, 89, 23);
+		btnLigarArCondicionado.setBounds(152, 300, 89, 23);
+		btnDesligarArCondicionado.setBounds(251, 300, 89, 23);
 
-		JLabel lblTitulo = new JLabel("Controle Remoto");
-		lblTitulo.setBounds(60, 34, 230, 23);
+                
+		/*-----------------------------------------------------------
+                ADICIONANDO OS COMPONENTES NO JFRAME
+                -----------------------------------------------------------*/
+		add(lblTitulo);
+                add(lblTv);
+                add(lblRadio);
+                add(lblPortaoEletronico);
+                add(lblArCondicionado);
+                
+                add(lblResposta);
+                add(lblHistorico);
+                
+                add(btnLigarTV);
+                add(btnDesligarTV);
+                add(btnLigarRadio);
+                add(btnDesligarRadio);
+                add(btnLigarPortaoEletronico);
+                add(btnDesligarPortaoEletronico);
+                add(btnLigarArCondicionado);
+                add(btnDesligarArCondicionado);
+               
+                
+                /*-----------------------------------------------------------
+                ALINHAMENTO DOS COMPONENTES
+                -----------------------------------------------------------*/
 		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTitulo.setFont(new Font("Arial Black", Font.PLAIN, 22));
-		add(lblTitulo);
-
-		JLabel lblTv = new JLabel("TV");
-		lblTv.setBounds(10, 102, 132, 23);
 		lblTv.setHorizontalAlignment(SwingConstants.CENTER);
-		add(lblTv);
-
-		JButton btnLigarTV = new JButton("Ligar");
-		btnLigarTV.setBounds(152, 102, 89, 23);
-		add(btnLigarTV);
-
-		JButton btnDesligarTV = new JButton("Desligar");
-		btnDesligarTV.setBounds(251, 102, 89, 23);
-		add(btnDesligarTV);
-
-		JLabel lblRadio = new JLabel("R�dio");
-		lblRadio.setBounds(10, 172, 132, 23);
 		lblRadio.setHorizontalAlignment(SwingConstants.CENTER);
-		add(lblRadio);
-
-		JButton btnLigarRadio = new JButton("Ligar");
-		btnLigarRadio.setBounds(152, 172, 89, 23);
-		add(btnLigarRadio);
-
-		JButton btnDesligarRadio = new JButton("Desligar");
-		btnDesligarRadio.setBounds(251, 172, 89, 23);
-		add(btnDesligarRadio);
-
-		JLabel lblPortaoEletronico = new JLabel("Port�o Eletr�nico");
-		lblPortaoEletronico.setBounds(10, 237, 132, 23);
 		lblPortaoEletronico.setHorizontalAlignment(SwingConstants.CENTER);
-		add(lblPortaoEletronico);
-
-		JButton btnLigarPortaoEletronico = new JButton("Ligar");
-		btnLigarPortaoEletronico.setBounds(152, 237, 89, 23);
-		add(btnLigarPortaoEletronico);
-
-		JButton btnDesligarPortaoEletronico = new JButton("Desligar");
-		btnDesligarPortaoEletronico.setBounds(251, 237, 89, 23);
-		add(btnDesligarPortaoEletronico);
-
-		JLabel lblArCondicionado = new JLabel("Ar Condicionado");
-		lblArCondicionado.setBounds(10, 300, 132, 23);
 		lblArCondicionado.setHorizontalAlignment(SwingConstants.CENTER);
-		add(lblArCondicionado);
-
-		JButton btnLigarArCondicionado = new JButton("Ligar");
-		btnLigarArCondicionado.setBounds(152, 300, 89, 23);
-		add(btnLigarArCondicionado);
-
-		JButton btnDesligarArCondicionado = new JButton("Desligar");
-		btnDesligarArCondicionado.setBounds(251, 300, 89, 23);
-		add(btnDesligarArCondicionado);
+		
+                
+                /*-----------------------------------------------------------
+                CONFIGURAÇÃO INICIAIS DOS BOTÕES
+                -----------------------------------------------------------*/
+                btnDesligarTV.setEnabled(false);
+                btnDesligarRadio.setEnabled(false);
+                btnDesligarPortaoEletronico.setEnabled(false);
+                btnDesligarArCondicionado.setEnabled(false);
+            
+                
+                /*-----------------------------------------------------------
+                CONFIGURAÇÃO DA JANELA
+                -----------------------------------------------------------*/
+                setBackground(new Color(169, 169, 169));
+		setLayout(null);
+                setVisible(true);
+                setSize(400,500);
+                setDefaultCloseOperation(EXIT_ON_CLOSE);
+                
+                
+                /*-----------------------------------------------------------
+                FUNÇÕES DOS BOTÕES
+                -----------------------------------------------------------*/
+                btnLigarTV.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btnLigarTV.setEnabled(false);
+                                btnDesligarTV.setEnabled(true);
+                                
+                                gerenciar.ArmazenarEAcionar(new BotaoLigar(), tv);
+                                lblResposta.setText("TV On");
+                                lblHistorico.setText(gerenciar.listarHistorico());
+			}
+		}); 
+                
+                 btnDesligarTV.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btnLigarTV.setEnabled(true);
+                                btnDesligarTV.setEnabled(false);
+                                
+                                gerenciar.ArmazenarEAcionar(new BotaoDesligar(), tv);
+                                lblResposta.setText("TV Off");
+                                lblHistorico.setText(gerenciar.listarHistorico());
+			}
+		});
+                 
+                btnLigarRadio.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btnLigarRadio.setEnabled(false);
+                                btnDesligarRadio.setEnabled(true);
+                                
+                                gerenciar.ArmazenarEAcionar(new BotaoDesligar(), radio);
+                                lblResposta.setText("Radio On");
+                                lblHistorico.setText(gerenciar.listarHistorico());
+			}
+		});
+                  
+                btnDesligarRadio.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btnLigarRadio.setEnabled(true);
+                                btnDesligarRadio.setEnabled(false);
+                                
+                                gerenciar.ArmazenarEAcionar(new BotaoDesligar(), radio);
+                                lblResposta.setText("Radio Off");
+                                lblHistorico.setText(gerenciar.listarHistorico());
+			}
+		});
+                
+                
+                btnLigarPortaoEletronico.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btnLigarPortaoEletronico.setEnabled(false);
+                                btnDesligarPortaoEletronico.setEnabled(true);
+                                
+                                gerenciar.ArmazenarEAcionar(new BotaoDesligar(), portaoEletronico);
+                                lblResposta.setText("Portao Eletronico On");
+                                lblHistorico.setText(gerenciar.listarHistorico());
+			}
+		});
+                  
+                btnDesligarPortaoEletronico.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btnLigarPortaoEletronico.setEnabled(true);
+                                btnDesligarPortaoEletronico.setEnabled(false);
+                                
+                                gerenciar.ArmazenarEAcionar(new BotaoDesligar(), portaoEletronico);
+                                lblResposta.setText("Portao Eletronico Off");
+                                lblHistorico.setText(gerenciar.listarHistorico());
+			}
+		});
+                
+                btnLigarArCondicionado.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btnLigarArCondicionado.setEnabled(false);
+                                btnDesligarArCondicionado.setEnabled(true);
+                                
+                                gerenciar.ArmazenarEAcionar(new BotaoDesligar(),arCondicionado);
+                                lblResposta.setText("Ar Condicionado On");
+                                lblHistorico.setText(gerenciar.listarHistorico());
+			}
+		});
+                  
+                btnDesligarArCondicionado.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btnLigarArCondicionado.setEnabled(true);
+                                btnDesligarArCondicionado.setEnabled(false);
+                                
+                                gerenciar.ArmazenarEAcionar(new BotaoDesligar(), arCondicionado);
+                                lblResposta.setText("Ar Condicionado Off");
+                                lblHistorico.setText(gerenciar.listarHistorico());
+			}
+		});
 	}
 	
 	public static void main(String[] args) {
-		JanelaPrincipal janela new JanelaPrincipal();
+            new JanelaPrincipal();
 	}
 }
